ed93cd57c37bc0bf0e2b90a2674068e891c612a3
Lucas Vieira <lucas.engen.cc@gmail.com>
2019-11-21 21:01:43 -0300

    Módulo simples, exibe uma mensagem no log do sistema
    Signed-off-by: Lucas Vieira <lucas.engen.cc@gmail.com>

---
+++ hello.c
@@ -0,0 +1,24 @@
+/*
+ *
+ * Estudo sobre módulos do linux kernel
+ *
+ * Autor: Lucas Vieira
+ */
+
+#include <linux/kernel.h>
+#include <linux/module.h>
+#include <linux/init.h>
+
+static int on_start()
+{
+	printk(KERN_INFO "Inicializando o modulo ...\n");
+	return -1;
+}
+
+static void on_finish()
+{
+	printk(KERN_INFO "Finalizando o módulo ...\n");
+}
+
+module_init(on_start);
+module_exit(on_finish);
